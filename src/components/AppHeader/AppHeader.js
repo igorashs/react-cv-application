3b3336ab7557c873e1ca061962145ab1237c4550
igorashs <igorashsirbu@gmail.com>
2020-12-03 17:32:24 +0200

    rewrite AppHeader with hooks

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { AppBar, Box, Typography } from '@material-ui/core';
 
-export default class AppHeader extends React.Component {
-  render() {
-    return (
-      <AppBar>
-        <Box p={1.25}>
-          <Typography variant='h4' component='h1'>
-            CV-Application
-          </Typography>
-          <Typography variant='subtitle1' component='h2'>
-            Make your CV!
-          </Typography>
-        </Box>
-      </AppBar>
-    );
-  }
+export default function AppHeader() {
+  return (
+    <AppBar>
+      <Box p={1.25}>
+        <Typography variant='h4' component='h1'>
+          CV-Application
+        </Typography>
+        <Typography variant='subtitle1' component='h2'>
+          Make your CV!
+        </Typography>
+      </Box>
+    </AppBar>
+  );
 }
